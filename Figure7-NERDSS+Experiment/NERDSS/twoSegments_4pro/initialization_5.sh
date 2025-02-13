6ec9298c5174b5df767f053a9853cb350796fc77
sangmk <msang2@jhu.edu>
2025-02-13 11:03:51 -0500

    sync changes

---
+++ Figure7-NERDSS+Experiment/NERDSS/twoSegments_4pro/initialization_5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=EquiSpatial
+#SBATCH --time=72:0:0
+#SBATCH --partition=parallel
+#SBATCH --nodes=1
+# number of tasks (processes) per node
+#SBATCH --ntasks-per-node=48
+#### load and unload modules you may need
+
+module load gsl 
+
+pfolders="kppE5.0kpnE1.0"
+restartfile="restart.dat"
+# parmfile="parms.inp"
+# coordfile="coordinate_fix.inp"
+current_dir=`pwd`
+
+for dir in $pfolders
+do 
+    for i in {0..47}
+    do
+        cd $dir/$i
+        rm -f nerdss
+        cp /home/msang2/mankun/nerdss_development/bin/nerdss ./nerdss
+        sed -i -e 's/pdbWrite = .*/pdbWrite = 10000000/' $restartfile
+        ./nerdss -r $restartfile > OUTPUT &
+        cd $current_dir
+    done
+done
+wait
+# done
