7ff06f284364881d42d55d79b99459be96d390c4
sangmk <msang2@jhu.edu>
2025-03-11 15:37:03 -0400

    nerdss -o1 does not work

--- Figure7-NERDSS+Experiment/NERDSS/fourDNAs/22targets/runjob.sh
+++ Figure7-NERDSS+Experiment/NERDSS/fourDNAs/22targets/runjob.sh
@@ -1,12 +1,16 @@
 for i in {0..29}
 do
-    mkdir $i
+    mkdir -p $i # Create a directory for each job
     cp *.mol ./$i
     cp parms.inp ./$i
     cp restart.dat ./$i
     cd $i
     rm -f nerdss
     ln -s ~/mankun/nerdss_development/bin/nerdss ./
+    rm -rf PDB
+    rm -rf RESTARTS
+    rm -rf DATA
     nohup ./nerdss -r restart.dat > OUTPUT &
+    # bash ../runNERDSSandMonitor.sh $PWD &
     cd ..
 done
