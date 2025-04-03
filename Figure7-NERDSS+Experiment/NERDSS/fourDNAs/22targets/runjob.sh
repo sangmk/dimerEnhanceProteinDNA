2fd1a5c248f96a6bf478c708a0a723e882a6717e
sangmk <msang2@jhu.edu>
2025-04-02 22:39:37 -0400

    sync edits

--- Figure7-NERDSS+Experiment/NERDSS/fourDNAs/22targets/runjob.sh
+++ Figure7-NERDSS+Experiment/NERDSS/fourDNAs/22targets/runjob.sh
@@ -1,16 +1,19 @@
-for i in {0..29}
+for i in {0..14}
 do
+    rm -rf ./$i
     mkdir -p $i # Create a directory for each job
     cp *.mol ./$i
     cp parms.inp ./$i
-    cp restart.dat ./$i
+    cp fixCoordinates.inp ./$i
+    # cp restart.dat ./$i
     cd $i
-    rm -f nerdss
-    ln -s ~/mankun/nerdss_development/bin/nerdss ./
-    rm -rf PDB
-    rm -rf RESTARTS
-    rm -rf DATA
-    nohup ./nerdss -r restart.dat > OUTPUT &
+    # rm -f nerdss
+    cp ~/mankun/nerdss_development/bin/nerdss ./nerdss
+    # rm -rf PDB
+    # rm -rf RESTARTS
+    # rm -rf DATA
+    # nohup ./nerdss -r restart.dat > OUTPUT &
+    nohup ./nerdss -f parms.inp -c fixCoordinates.inp > OUTPUT &
     # bash ../runNERDSSandMonitor.sh $PWD &
     cd ..
 done
