e1f2425af8c984e49d82663258f904e19ef37326
sangmk <msang2@jhu.edu>
2025-06-30 13:42:42 -0400

    remove 22targets simulation files

--- Figure7-NERDSS+Experiment/NERDSS/twoSegments_4pro/copyfiles.sh
+++
@@ -1,6 +0,0 @@
-folder=kppE6.0kpnE1.0
-# folder=kpp0kpnE1.0
-mkdir -p $folder
-path=msang2@login.rockfish.jhu.edu:/home/msang2/mankun/twoSegments_4pro/
-# scp $path/$folder/0/restart.dat $path/$folder/0/*.inp $path/$folder/0/*.mol $folder
-scp $path/$folder/0/restart.dat $path/$folder/0/*.inp $path/$folder/0/*.mol $folder
