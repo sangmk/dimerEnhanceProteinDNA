4d6d8bbca5c5410db18b2f02ad75979a68ff4052
msang2 <msang2@login01.cm.cluster>
2025-10-16 18:46:04 -0400

    16 DNA kinetics

--- Figure2-DNA/NERDSS_a/16DNA/kpp1E6koffP10/submitjob.sh
+++ Figure2-DNA/NERDSS_a/16DNA/kpp1E6koffP10/submitjob.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=kbp10
+#SBATCH --job-name=kbp10_16DNA
 #SBATCH --time=72:0:0
 #SBATCH --partition=parallel
 #SBATCH --nodes=1
@@ -15,14 +15,19 @@ current_dir=`pwd`
 
 for i in {0..47}
 do
+    # prepare directory
     rm -rf nerdss_output/$i
     mkdir -p nerdss_output/$i
+    # prepare NERDSS
     cp nerdss_input/parms.inp nerdss_output/$i
     cp nerdss_input/N.mol nerdss_output/$i
     cp nerdss_input/P.mol nerdss_output/$i
+    cp nerdss_input/fixCoordinates.pdb nerdss_output/$i
     cd ./nerdss_output/$i
     cp /home/msang2/mankun/nerdss_development/bin/nerdss ./nerdss
-    ./nerdss -f parms.inp > OUTPUT &
+    # run NERDSS
+    ./nerdss -f parms.inp -c fixCoordinates.pdb > OUTPUT &
+    # go back to current directroy
     cd $current_dir
 done
 wait
