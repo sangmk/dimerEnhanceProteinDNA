569ac33ca640a91c74f9b416f99e6d612d23ea20
msang2 <msang2@login02.cm.cluster>
2025-10-16 12:26:38 -0400

    rockfish simulations

---
+++ Figure2-DNA/NERDSS_a/kpp1E6koffP1/submitjob.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=kbp1
+#SBATCH --time=72:0:0
+#SBATCH --partition=parallel
+#SBATCH --nodes=1
+# number of tasks (processes) per node
+#SBATCH --ntasks-per-node=48
+#### load and unload modules you may need
+
+module load gsl 
+
+
+current_dir=`pwd`
+
+
+for i in {0..47}
+do
+    rm -rf nerdss_output/$i
+    mkdir -p nerdss_output/$i
+    cp nerdss_input/parms.inp nerdss_output/$i
+    cp nerdss_input/N.mol nerdss_output/$i
+    cp nerdss_input/P.mol nerdss_output/$i
+    cd ./nerdss_output/$i
+    cp /home/msang2/mankun/nerdss_development/bin/nerdss ./nerdss
+    ./nerdss -f parms.inp > OUTPUT &
+    cd $current_dir
+done
+wait
+# done
---
+++ Figure2-DNA/NERDSS_a/submitAllJobs.sh
@@ -0,0 +1,19 @@
+cd dimer
+sbatch submitjob.sh
+cd ..
+
+cd monomer
+sbatch submitjob.sh
+cd ..
+
+cd kpp1E6koffP01
+sbatch submitjob.sh
+cd ..
+
+cd kpp1E6koffP1
+sbatch submitjob.sh
+cd ..
+
+cd kpp1E6koffP10
+sbatch submitjob.sh
+cd ..
